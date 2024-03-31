929522d19f4024edd1acaa11812a5d5a43c6ba83
ktydn <ktydn1995@gmail.com>
2024-04-01 00:58:08 +0900

    update: トップページにリンク追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+import Link from 'next/link';
+
 export default function Page() {
-  return <h1>Top Page</h1>
+  return (
+    <div>
+      <h1>Top Page</h1>
+      <Link href="/layout-test">layout-test</Link><br />
+      <Link href="/loading-test">loading-test</Link><br />
+      <Link href="/page-test">page-test</Link><br />
+      <Link href="/template-test">template-test</Link><br />
+    </div>
+  );
 }
