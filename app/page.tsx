4b2cfc7523b72f14b1e63a3a5bb17afdfc74b6d5
ktydn <ktydn1995@gmail.com>
2024-04-03 00:33:20 +0900

    add: api追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Link from 'next/link';
+import { Roboto } from 'next/font/google'
+const Roboto900 = Roboto({ weight:'900', preload:false })
 
 export default function Page() {
   return (
     <div>
-      <h1>Top Page</h1>
+      <h1 className={Roboto900.className}>Top Page</h1>
       <Link href="/layout-test">layout-test</Link><br />
       <Link href="/loading-test">loading-test</Link><br />
       <Link href="/page-test">page-test</Link><br />
