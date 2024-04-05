17f250f480c1c4ab7088f5cd288f146b761d880f
ktydn <ktydn1995@gmail.com>
2024-04-06 00:38:37 +0900

    update: ディレクトリ整理

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,10 @@ export default function Page() {
     <div>
       <h1 className={Roboto900.className}>Top Page</h1>
       <Link href="/api/login">login</Link><br />
-      <Link href="/layout-test">layout-test</Link><br />
-      <Link href="/loading-test">loading-test</Link><br />
-      <Link href="/page-test">page-test</Link><br />
-      <Link href="/template-test">template-test</Link><br />
+      <Link href="/test/layout-test">layout-test</Link><br />
+      <Link href="/test/loading-test">loading-test</Link><br />
+      <Link href="/test/page-test">page-test</Link><br />
+      <Link href="/test/template-test">template-test</Link><br />
     </div>
   );
 }
