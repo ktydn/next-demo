a4c429640e905a588c8ac43eceeeebb06877c50f
ktydn <ktydn1995@gmail.com>
2024-04-06 00:06:21 +0900

    add: Google APIへの認証の仕組み追加
    https://qiita.com/Hirohana/items/e3b71af64311e26582a6

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ export default function Page() {
   return (
     <div>
       <h1 className={Roboto900.className}>Top Page</h1>
+      <Link href="/api/login">login</Link><br />
       <Link href="/layout-test">layout-test</Link><br />
       <Link href="/loading-test">loading-test</Link><br />
       <Link href="/page-test">page-test</Link><br />
---
+++ lib/auth.ts
@@ -0,0 +1,38 @@
+import type { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    jwt: ({ token, user }) => {
+      if (user) {
+        const u = user as unknown as any;
+        return {
+          ...token,
+          id: u.id,
+        };
+      }
+      return token;
+    },
+    session: ({ session, token }) => {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: token.id,
+        },
+      };
+    },
+  },
+};
