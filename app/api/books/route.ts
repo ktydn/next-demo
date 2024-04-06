3f72045b2952f02e4a12f06910ec2adcb327a85e
ktydn <ktydn1995@gmail.com>
2024-04-06 10:21:12 +0900

    add: prettier導入

--- app/api/books/route.ts
+++ app/api/books/route.ts
@@ -1,13 +1,11 @@
-var books = [
-    { title: '吾輩は猫である', author: '夏目漱石' },
-  ]
-  
-  export async function GET(request: Request) {
-    return Response.json({ books: books }, { status: 200 })
-  }
-  
-  export async function POST(request: Request) {
-    var book = await request.json()
-    books.push({ title: book.title, author: book.author })
-    return Response.json({ result: 'OK' }, { status: 200 })
-  }
+var books = [{ title: "吾輩は猫である", author: "夏目漱石" }];
+
+export async function GET(request: Request) {
+  return Response.json({ books: books }, { status: 200 });
+}
+
+export async function POST(request: Request) {
+  var book = await request.json();
+  books.push({ title: book.title, author: book.author });
+  return Response.json({ result: "OK" }, { status: 200 });
+}
--- app/test/loading-test/wait.tsx
+++ app/test/loading-test/wait.tsx
@@ -1,7 +1,9 @@
 function aWait() {
-    return new Promise(callback => { setTimeout(callback, 3000) })
-  }
-  export default async function WaitComponent() {
-    await aWait()
-    return <div>Finished!</div>
-  }
+  return new Promise((callback) => {
+    setTimeout(callback, 3000);
+  });
+}
+export default async function WaitComponent() {
+  await aWait();
+  return <div>Finished!</div>;
+}
