cf21eaa44c2802af99051335c21f2561e048dad0
ktydn <ktydn1995@gmail.com>
2024-04-06 22:12:06 +0900

    add: profile用css追加

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,32 +1,56 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import Image from "next/image";
+import "@/app/profile-template.css";
 
 export default async function Profile() {
   const session = await getServerSession(authOptions);
   const user = session?.user;
 
   return (
-    <>
-      <p>Profile Page</p>
-      {!user ? (
-        <p>ユーザー情報が取得できていません。。。</p>
-      ) : (
-        <>
-          <div>
+    <div className="writer-profile-card">
+      <div className="wp-left">
+        <div className="wp-label">
+          <p>自己紹介</p>
+        </div>
+        <div className="wp-img">
+          {user ? (
             <Image
               src={user.image ? user.image : "/images/default.png"}
-              width={150}
-              height={150}
+              width={100}
+              height={100}
               alt={`profile photo of ${user.name}`}
             />
-          </div>
-          <div className="mt-8">
-            <p className="mb-3">Name: {user.name}</p>
-            <p className="mb-3">Email: {user.email}</p>
-          </div>
-        </>
-      )}
-    </>
+          ) : (
+            <p>画像がありません</p>
+          )}
+        </div>
+        <p className="wp-name">
+          {user ? user.name : "ユーザー情報が取得できていません"}
+        </p>
+      </div>
+      <div className="wp-right">
+        <div className="ul-center">
+          <ul>
+            {user ? (
+              <>
+                <li className="li-point">名前: {user.name}</li>
+                <li className="li-point">Email: {user.email}</li>
+                {/* 他のユーザー情報を表示する */}
+              </>
+            ) : (
+              <li className="li-point">ユーザー情報が取得できていません</li>
+            )}
+          </ul>
+        </div>
+        <ul className="wp-sns">
+          <li>
+            <a href="ここにリンク先URL">
+              <i className="fa fa-link" aria-hidden="true"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
