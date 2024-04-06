099844dfbd2b071d8facf3245105bc8561b73038
ktydn <ktydn1995@gmail.com>
2024-04-06 20:41:23 +0900

    fix: <suspense>で囲う

--- app/api/login/page.tsx
+++ app/api/login/page.tsx
@@ -2,10 +2,19 @@
 
 import { signIn } from "next-auth/react"; // 1⃣
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function Login() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <LoginForm />
+    </Suspense>
+  );
+}
+
+function LoginForm() {
   const searchParams = useSearchParams();
-  const callbackUrl = searchParams.get("callbackUrl") || "/profile"; // 2⃣
+  const callbackUrl = searchParams.get("callbackUrl") || "/profile";
 
   return (
     <button onClick={() => signIn("google", { callbackUrl })}>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  pageExtensions: ["tsx", "mdx", "ts", "js"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
