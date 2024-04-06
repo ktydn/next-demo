96e9d1666f8d42f89e23a36278fcba62ec82d58c
ktydn <ktydn1995@gmail.com>
2024-04-06 20:55:05 +0900

    fix: nextauthにsecret設定

--- lib/auth.ts
+++ lib/auth.ts
@@ -8,6 +8,7 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
   pages: {
     signIn: "/login",
   },
